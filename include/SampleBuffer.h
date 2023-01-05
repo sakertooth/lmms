d4a61c7bb1232ef6e33434ab70ddbfbac70e7396
sakertooth <sakertooth@gmail.com>
2023-01-05 18:33:03 -0500

    Organize header file Move related functions (such as getters & setters and manipulators) so they are together
    This commit moves MARGIN into the implementation file,
    while also providing a function interpolationMargins.

--- include/SampleBuffer.h
+++ include/SampleBuffer.h
@@ -47,13 +47,6 @@ class QRect;
 
 namespace lmms
 {
-
-// values for buffer margins, used for various libsamplerate interpolation modes
-// the array positions correspond to the converter_type parameter values in libsamplerate
-// if there appears problems with playback on some interpolation mode, then the value for that mode
-// may need to be higher - conversely, to optimize, some may work with lower values
-const auto MARGIN = std::array<f_cnt_t, 5>{ 64, 64, 64, 4, 4 };
-
 class LMMS_EXPORT SampleBuffer : public QObject, public sharedObject
 {
 	Q_OBJECT
@@ -61,135 +54,88 @@ class LMMS_EXPORT SampleBuffer : public QObject, public sharedObject
 public:
 	enum class LoopMode
 	{
-		LoopOff,
-		LoopOn,
-		LoopPingPong
+		LoopOff, LoopOn, LoopPingPong
 	};
-	
-	SampleBuffer();
-	// constructor which either loads sample _audio_file or decodes
-	// base64-data out of string
-	SampleBuffer(const QString & audioFile, bool isBase64Data = false);
-	SampleBuffer(const sampleFrame * data, const f_cnt_t frames);
-	explicit SampleBuffer(const f_cnt_t frames);
-	SampleBuffer(const SampleBuffer & orig);
 
-	friend void swap(SampleBuffer & first, SampleBuffer & second) noexcept;
-	SampleBuffer& operator=(const SampleBuffer that);
+	SampleBuffer();
+	SampleBuffer(const QString& audioFile, bool isBase64Data = false);
+	SampleBuffer(const sampleFrame* data, f_cnt_t frames);
+	explicit SampleBuffer(f_cnt_t frames);
+	SampleBuffer(const SampleBuffer& orig);
 
 	~SampleBuffer() override;
 
-	bool play(
-		sampleFrame * ab,
-		Sample * state,
-		const fpp_t frames,
-		const float freq,
-		const LoopMode loopMode = LoopMode::LoopOff
-	);
+	friend void swap(SampleBuffer& first, SampleBuffer& second) noexcept;
+	SampleBuffer& operator=(const SampleBuffer that);
 
-	void visualize(
-		QPainter & p,
-		const QRect & dr,
-		const QRect & clip,
-		f_cnt_t fromFrame = 0,
-		f_cnt_t toFrame = 0
-	);
+	bool play(sampleFrame* dst, Sample* state, fpp_t frames, float freq, LoopMode loopMode = LoopMode::LoopOff);
+	void visualize(QPainter& p, const QRect& dr, const QRect& clip, f_cnt_t fromFrame = 0, f_cnt_t toFrame = 0);
+	void visualize(QPainter& p, const QRect& dr, f_cnt_t fromFrame = 0, f_cnt_t toFrame = 0);
 
-	void visualize(
-		QPainter & p,
-		const QRect & dr,
-		f_cnt_t fromFrame = 0,
-		f_cnt_t toFrame = 0
-	);
+	SampleBuffer* resample(sample_rate_t srcSR, sample_rate_t dstSR);
+	void normalizeSampleRate(sample_rate_t srcSR, bool keepSettings = false);
+	sample_t userWaveSample(float sample) const;
+	int sampleLength() const;
+	QString toBase64() const;
 
-	const QString & audioFile() const;
+	static const std::array<f_cnt_t, 5>& interpolationMargins();
 
-	f_cnt_t startFrame() const;
-	f_cnt_t endFrame() const;
-	f_cnt_t loopStartFrame() const;
-
-	f_cnt_t loopEndFrame() const;
-	void setLoopStartFrame(f_cnt_t start);
-	void setLoopEndFrame(f_cnt_t end);
-	void setAllPointFrames(f_cnt_t start, f_cnt_t end, f_cnt_t loopStart, f_cnt_t loopEnd);
-
-	std::shared_mutex& mutex();
-
-	f_cnt_t frames() const;
+	const QString& audioFile() const;
+	const sampleFrame* data() const;
+	std::shared_mutex& mutex() const;
 	float amplification() const;
 	bool reversed() const;
 	float frequency() const;
 	sample_rate_t sampleRate() const;
 	const std::unique_ptr<OscillatorConstants::waveform_t>& userAntiAliasWaveTable() const;
 
-	int sampleLength() const;
-
-	void setFrequency(float freq);
-	void setSampleRate(sample_rate_t rate);
-	const sampleFrame * data() const;
-
-	QString toBase64() const;
-
-	// protect calls from the GUI to this function with dataReadLock() and
-	// dataUnlock()
-	SampleBuffer * resample(const sample_rate_t srcSR, const sample_rate_t dstSR);
-
-	void normalizeSampleRate(const sample_rate_t srcSR, bool keepSettings = false);
-
-	// protect calls from the GUI to this function with dataReadLock() and
-	// dataUnlock(), out of loops for efficiency
-	sample_t userWaveSample(const float sample) const;
-
-public slots:
-	void setAudioFile(const QString & audioFile);
-	void loadFromBase64(const QString & data);
-	void setStartFrame(const lmms::f_cnt_t s);
-	void setEndFrame(const lmms::f_cnt_t e);
-	void setAmplification(float a);
-	void setReversed(bool on);
-	void sampleRateChanged();
+	f_cnt_t frames() const;
+	f_cnt_t startFrame() const;
+	f_cnt_t endFrame() const;
+	f_cnt_t loopStartFrame() const;
+	f_cnt_t loopEndFrame() const;
 
 signals:
 	void sampleUpdated();
 
+public slots:
+	void setAudioFile(const QString& audioFile);
+	void loadFromBase64(const QString& data);
+
+	void setStartFrame(f_cnt_t startFrame);
+	void setEndFrame(f_cnt_t endFrame);
+	void setLoopStartFrame(f_cnt_t loopStart);
+	void setLoopEndFrame(f_cnt_t loopEnd);
+	void setAllPointFrames(f_cnt_t start, f_cnt_t end, f_cnt_t loopStart, f_cnt_t loopEnd);
+	
+	void setAmplification(float amplification);
+	void setReversed(bool on);
+	void setFrequency(float frequency);
+	void setSampleRate(sample_rate_t sampleRate);
+	void sampleRateChanged();
+
 private:
+	void update(bool keepSettings = false);
+	void convertIntToFloat(int_sample_t*& ibuf, f_cnt_t frames, int channels);
+	void directFloatWrite(sample_t*& fbuf, f_cnt_t frames, int channels);
+	bool fileExceedsLimits(const QString& audioFile, bool reportToGui = true);
 	sample_rate_t audioEngineSampleRate();
 
-	void update(bool keepSettings = false);
+	f_cnt_t decodeSampleSF(QString fileName, sample_t*& buf, ch_cnt_t& channels, sample_rate_t& sampleRate);
+	f_cnt_t decodeSampleDS(QString fileName, int_sample_t*& buf, ch_cnt_t& channels, sample_rate_t& sampleRate);
 
-	void convertIntToFloat(int_sample_t * & ibuf, f_cnt_t frames, int channels);
-	void directFloatWrite(sample_t * & fbuf, f_cnt_t frames, int channels);
-
-	bool fileExceedsLimits(const QString& audioFile, bool reportToGui = true);
-
-	f_cnt_t decodeSampleSF(
-		QString fileName,
-		sample_t * & buf,
-		ch_cnt_t & channels,
-		sample_rate_t & samplerate
-	);
-
-	f_cnt_t decodeSampleDS(
-		QString fileName,
-		int_sample_t * & buf,
-		ch_cnt_t & channels,
-		sample_rate_t & samplerate
-	);
-
-	sampleFrame * getSampleFragment(
+	sampleFrame* getSampleFragment(
 		f_cnt_t index,
 		f_cnt_t frames,
 		LoopMode loopMode,
-		sampleFrame * * tmp,
-		bool * backwards,
+		sampleFrame ** tmp,
+		bool* backwards,
 		f_cnt_t loopStart,
 		f_cnt_t loopEnd,
-		f_cnt_t end
-	) const;
+		f_cnt_t end) const;
 
-	f_cnt_t getLoopedIndex(f_cnt_t index, f_cnt_t startf, f_cnt_t endf) const;
-	f_cnt_t getPingPongIndex(f_cnt_t index, f_cnt_t startf, f_cnt_t endf) const;
-
+	f_cnt_t getLoopedIndex(f_cnt_t index, f_cnt_t startFrame, f_cnt_t endFrame) const;
+	f_cnt_t getPingPongIndex(f_cnt_t index, f_cnt_t startFrame, f_cnt_t endFrame) const;
 private:
 	QString m_audioFile = "";
 	sampleFrame* m_origData = nullptr;
