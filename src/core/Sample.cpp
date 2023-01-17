05d67683cd0f3214b435bf26a6915e5e9cb43d26
sakertooth <sakertooth@gmail.com>
2023-01-17 08:13:30 -0500

    Move play function into Sample This also moves the following dependencies: * advance * getSampleFragment * getLoopedIndex * getPingPongIndex

--- src/core/Sample.cpp
+++ src/core/Sample.cpp
@@ -23,10 +23,265 @@
  */
 
 #include "Sample.h"
+
+#include "AudioEngine.h"
+#include "Engine.h"
+#include "SampleBuffer.h"
+
 #include <iostream>
 
 namespace lmms
 {
+    // values for buffer margins, used for various libsamplerate interpolation modes
+    // the array positions correspond to the converter_type parameter values in libsamplerate
+    // if there appears problems with playback on some interpolation mode, then the value for that mode
+    // may need to be higher - conversely, to optimize, some may work with lower values
+    static auto s_interpolationMargins = std::array<f_cnt_t, 5>{64, 64, 64, 4, 4};
+
+    auto Sample::play(
+        SampleBuffer* buffer, sampleFrame* dst, PlaybackState* state, 
+        fpp_t frames, float freq,
+        LoopMode loopMode
+    ) -> bool
+    {
+        const auto [startFrame, endFrame, loopStartFrame, loopEndFrame] = m_playMarkers;
+
+        if (endFrame == 0 || frames == 0)
+        {
+            return false;
+        }
+
+        // variable for determining if we should currently be playing backwards in a ping-pong loop
+        bool isBackwards = state->isBackwards();
+
+        // The SampleBuffer can play a given sample with increased or decreased pitch. However, only
+        // samples that contain a tone that matches the default base note frequency of 440 Hz will
+        // produce the exact requested pitch in [Hz].
+        const double freqFactor = (double) freq / (double) m_frequency *
+            buffer->sampleRate() / Engine::audioEngine()->processingSampleRate();
+
+        // calculate how many frames we have in requested pitch
+        const auto totalFramesForCurrentPitch = static_cast<f_cnt_t>((endFrame - startFrame) / freqFactor);
+
+        if (totalFramesForCurrentPitch == 0)
+        {
+            return false;
+        }
+
+
+        // this holds the index of the first frame to play
+        f_cnt_t playFrame = std::max(state->m_frameIndex, startFrame);
+
+        if (loopMode == LoopMode::LoopOff)
+        {
+            if (playFrame >= endFrame || (endFrame - playFrame) / freqFactor == 0)
+            {
+                // the sample is done being played
+                return false;
+            }
+        }
+        else if (loopMode == LoopMode::LoopOn)
+        {
+            playFrame = getLoopedIndex(playFrame, loopStartFrame, loopEndFrame);
+        }
+        else
+        {
+            playFrame = getPingPongIndex(playFrame, loopStartFrame, loopEndFrame);
+        }
+
+        f_cnt_t fragmentSize = (f_cnt_t)(frames * freqFactor) + s_interpolationMargins[state->interpolationMode()];
+
+        // check whether we have to change pitch...
+        if (freqFactor != 1.0 || state->m_varyingPitch)
+        {
+            SRC_DATA srcData;
+            // Generate output
+            const auto sampleFragment = getSampleFragment(buffer, playFrame, fragmentSize, loopMode, &isBackwards,
+                loopStartFrame, loopEndFrame, endFrame);
+            srcData.data_in = &sampleFragment[0][0];
+            srcData.data_out = dst->data();
+            srcData.input_frames = fragmentSize;
+            srcData.output_frames = frames;
+            srcData.src_ratio = 1.0 / freqFactor;
+            srcData.end_of_input = 0;
+            int error = src_process(state->m_resamplingData, &srcData);
+            if (error)
+            {
+                std::cerr << "SampleBuffer: error while resampling: " << src_strerror(error) << '\n';
+            }
+
+            if (srcData.output_frames_gen > frames)
+            {
+                std::cerr << "SampleBuffer: not enough frames: " << srcData.output_frames_gen << " / " << frames << '\n';
+            }
+
+            playFrame = advance(playFrame, srcData.input_frames_used, loopMode, state);
+        }
+        else
+        {
+            // we don't have to pitch, so we just copy the sample-data
+            // as is into pitched-copy-buffer
+
+            // Generate output
+            const auto sampleFragment = getSampleFragment(buffer, playFrame, frames, loopMode, &isBackwards,
+                    loopStartFrame, loopEndFrame, endFrame);
+            std::copy_n(sampleFragment.begin(), frames, dst);
+            playFrame = advance(playFrame, frames, loopMode, state);
+        }
+
+        state->setBackwards(isBackwards);
+        state->setFrameIndex(playFrame);
+
+        for (fpp_t i = 0; i < frames; ++i)
+        {
+            dst[i][0] *= m_amplification;
+            dst[i][1] *= m_amplification;
+        }
+
+        return true;
+    }
+
+    auto Sample::advance(f_cnt_t playFrame, f_cnt_t frames, LoopMode loopMode, PlaybackState* state) -> f_cnt_t
+    {
+        switch (loopMode)
+        {
+            case LoopMode::LoopOff:
+                return playFrame + frames;
+            case LoopMode::LoopOn:
+                return frames + getLoopedIndex(playFrame, m_playMarkers.loopStartFrame, m_playMarkers.loopEndFrame);
+            case LoopMode::LoopPingPong:
+            {
+                f_cnt_t left = frames;
+                if (state->isBackwards())
+                {
+                    playFrame -= frames;
+                    if (playFrame < m_playMarkers.loopStartFrame)
+                    {
+                        left -= m_playMarkers.loopStartFrame - playFrame;
+                        playFrame = m_playMarkers.loopStartFrame;
+                    }
+                    else
+                    {
+                        left = 0;
+                    }
+                }
+
+                return left + getPingPongIndex(playFrame, m_playMarkers.loopStartFrame, m_playMarkers.loopEndFrame);
+            }
+            default:
+                return playFrame;
+        }
+    }
+
+    std::vector<sampleFrame> Sample::getSampleFragment(
+        SampleBuffer* buffer,
+        f_cnt_t currentFrame,
+        f_cnt_t numFramesRequested,
+        LoopMode loopMode,
+        bool * backwards,
+        f_cnt_t loopStart,
+        f_cnt_t loopEnd,
+        f_cnt_t endFrame
+    ) const
+    {
+        auto out = std::vector<sampleFrame>(numFramesRequested);
+        if (loopMode == LoopMode::LoopOff)
+        {
+            // Specify the number of frames to copy, starting at index
+            // If there are not enough frames to copy, copy the remaining frames
+            // (endFrame - currentFrame)
+            const auto numFramesToCopy = std::min(numFramesRequested, endFrame - currentFrame);
+            assert(numFramesToCopy >= 0);
+            std::copy_n(buffer->data().begin() + currentFrame, numFramesToCopy, out.begin());
+        }
+        else if (loopMode == LoopMode::LoopOn || loopMode == LoopMode::LoopPingPong)
+        {
+            // Will be used to track the current play position
+            // and how many frames we copied while looping
+            auto playPosition = currentFrame;
+            auto numFramesCopied = 0;
+
+            // Move playPosition to loopStart if playPosition has past or is currently at loopEnd
+            if (playPosition >= loopEnd) { playPosition = loopStart; }
+
+            while (numFramesCopied != numFramesRequested)
+            {
+                // If we do not have enough frames to copy, then one of these conditions must be true:
+                // 1. playPosition + numFramesRequested > loopEnd for LoopOn mode and LoopPingPong mode (non-backwards),
+                // 2. playPosition - numFramesRequested < loopStart for LoopPingPong mode (backwards),
+
+                // If any of the previous conditions are true, then we should only copy the remaining frames
+                // we have in the following ranges:
+                // 1. loopEnd - playPosition for LoopOn mode and LoopPingPongMode (non-backwards)
+                // 2. playPosition - loopStart for LoopPingPong mode (backwards).
+
+                // Otherwise, we want to copy numFramesRequested frames minus how many frames we already copied
+
+                const auto remainingFrames = (*backwards && loopMode == LoopMode::LoopPingPong) ?
+                    playPosition - loopStart : loopEnd - playPosition;
+                const auto numFramesToCopy = std::min(numFramesRequested - numFramesCopied, remainingFrames);
+                assert(numFramesToCopy >= 0);
+
+                if (loopMode == LoopMode::LoopOn || (!*backwards && loopMode == LoopMode::LoopPingPong))
+                {
+                    std::copy_n(buffer->data().begin() + playPosition, numFramesToCopy, out.begin() + numFramesCopied);
+                }
+                else if (*backwards && loopMode == LoopMode::LoopPingPong)
+                {
+                    auto distanceFromPlayPosition = std::distance(buffer->data().begin() + playPosition, buffer->data().end());
+                    std::copy_n(buffer->data().rbegin() + distanceFromPlayPosition, numFramesToCopy, out.begin() + numFramesCopied);
+                }
+
+                playPosition += (*backwards && loopMode == LoopMode::LoopPingPong ? -numFramesToCopy : numFramesToCopy);
+                numFramesCopied += numFramesToCopy;
+
+                // Ensure that playPosition is in the valid range [0, loopEnd]
+                assert(playPosition >= 0 && playPosition <= loopEnd);
+
+                // numFramesCopied can only be less than or equal to numFramesRequested
+                assert(numFramesCopied <= numFramesRequested);
+
+                if (playPosition == loopEnd && loopMode == LoopMode::LoopOn)
+                {
+                    playPosition = loopStart;
+                }
+                else if (playPosition == loopEnd && loopMode == LoopMode::LoopPingPong)
+                {
+                    *backwards = true;
+                }
+                else if (playPosition == loopStart && *backwards && loopMode == LoopMode::LoopPingPong)
+                {
+                    *backwards = false;
+                }
+            }
+        }
+        
+        return out;
+    }
+
+    f_cnt_t Sample::getPingPongIndex(f_cnt_t index, f_cnt_t startf, f_cnt_t endf) const
+    {
+        if (index < endf)
+        {
+            return index;
+        }
+        const f_cnt_t loopLen = endf - startf;
+        const f_cnt_t loopPos = (index - endf) % (loopLen * 2);
+
+        return (loopPos < loopLen)
+            ? endf - loopPos
+            : startf + (loopPos - loopLen);
+    }
+
+    f_cnt_t Sample::getLoopedIndex(f_cnt_t index, f_cnt_t startf, f_cnt_t endf) const
+    {
+        if (index < endf)
+        {
+            return index;
+        }
+        return startf + (index - startf) % (endf - startf);
+    }
+
     auto Sample::startFrame() const -> f_cnt_t
     {
         return m_playMarkers.startFrame;
