5687f5185bdf1fffe8f2e48a45d2bd3a6bfb4f71
Sotonye Atemie <sakertooth@gmail.com>
2025-02-11 03:31:20 -0500

    Add SampleStream

---
+++ include/SampleStream.h
@@ -0,0 +1,81 @@
+/*
+ * SampleStream.h
+ *
+ * Copyright (c) 2025 Sotonye Atemie <sakertooth@gmail.com>
+ *
+ * This file is part of LMMS - https://lmms.io
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program (see COPYING); if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301 USA.
+ *
+ */
+
+#ifndef LMMS_SAMPLE_STREAM_H
+#define LMMS_SAMPLE_STREAM_H
+
+#include <filesystem>
+#include <sndfile.h>
+
+#include "MemoryMappedFile.h"
+#include "SampleFrame.h"
+#include "lmms_basics.h"
+#include "lmms_export.h"
+
+namespace lmms {
+class LMMS_EXPORT SampleStream
+{
+public:
+	SampleStream(const std::filesystem::path& audioFilePath)
+		: m_memoryMappedFile(audioFilePath)
+		, m_sfVirtualIo(SF_VIRTUAL_IO{.get_filelen = &SampleStream::filelen,
+			  .seek = &SampleStream::seek,
+			  .read = &SampleStream::read,
+			  .write = &SampleStream::write,
+			  .tell = &SampleStream::tell})
+		, m_sndfile(sf_open_virtual(&m_sfVirtualIo, SFM_READ, &m_sfInfo, this))
+	{
+	}
+
+	void next(SampleFrame* ptr, std::size_t count) { sf_readf_float(m_sndfile, ptr->data(), count * DEFAULT_CHANNELS); }
+
+private:
+	static auto filelen(void* data) -> sf_count_t { return static_cast<MemoryMappedFile*>(data)->size(); }
+
+	static auto seek(sf_count_t offset, int whence, void* data) -> sf_count_t
+	{
+		return static_cast<MemoryMappedFile*>(data)->seek(offset, whence);
+	}
+
+	static auto read(void* ptr, sf_count_t count, void* data) -> sf_count_t
+	{
+		return static_cast<MemoryMappedFile*>(data)->read(ptr, count);
+	}
+
+	static auto write(const void* ptr, sf_count_t count, void* data) -> sf_count_t
+	{
+		return static_cast<MemoryMappedFile*>(data)->write(ptr, count);
+	}
+
+	static auto tell(void* data) -> sf_count_t { return static_cast<MemoryMappedFile*>(data)->tell(); }
+
+	MemoryMappedFile m_memoryMappedFile;
+	SF_VIRTUAL_IO m_sfVirtualIo;
+	SF_INFO m_sfInfo;
+	SNDFILE* m_sndfile;
+};
+
+} // namespace lmms
+
+#endif // LMMS_SAMPLE_BUFFER_H
