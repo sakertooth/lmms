e88ba60a50c9d8124035b0d3f9f151fd392f12d8
Sotonye Atemie <sakertooth@gmail.com>
2025-03-28 01:34:38 -0400

    Implement new audio graph class

---
+++ include/AudioGraph.h
@@ -0,0 +1,144 @@
+/*
+ * AudioGraph.h
+ *
+ * Copyright (c) 2025 Sotonye Atemie <sakertooth@gmail.com>
+ *
+ * This file is part of LMMS - https://lmms.io
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program (see COPYING); if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301 USA.
+ *
+ */
+
+#ifndef LMMS_AUDIO_GRAPH_H
+#define LMMS_AUDIO_GRAPH_H
+
+#include <memory>
+#include <thread>
+#include <unordered_map>
+#include <vector>
+
+#include "MixHelpers.h"
+#include "SampleFrame.h"
+
+namespace lmms {
+class AudioGraph
+{
+public:
+	class Node
+	{
+	protected:
+		class Token
+		{
+		};
+
+	public:
+		enum class State
+		{
+			Idle,
+			Queued,
+			Processing,
+			Processed,
+			Completed
+		};
+
+		Node(Token);
+		virtual ~Node() noexcept;
+
+		Node(const Node&) = default;
+		Node(Node&&) = delete;
+		Node& operator=(const Node&) = default;
+		Node& operator=(Node&&) = delete;
+
+		template <typename T>
+		static std::unique_ptr<T> create()
+		{
+			auto node = std::make_unique<T>(Token{});
+			AudioGraph::inst().add(node);
+			return node;
+		}
+
+		void send(Node* node) { sendImpl(node, node->m_buffer.data(), m_buffer.data(), m_buffer.size()); }
+		void send(SampleFrame* dst, std::size_t size) { sendImpl(nullptr, dst, m_buffer.data(), size); }
+		void process() { processImpl(m_buffer.data(), m_buffer.size()); }
+		bool finished() { return finishedImpl(); }
+
+	private:
+		virtual void sendImpl(Node* node, SampleFrame* dst, const SampleFrame* src, std::size_t size)
+		{
+			MixHelpers::add(m_buffer.data(), node->m_buffer.data(), m_buffer.size());
+		}
+
+		virtual void processImpl(SampleFrame* dst, std::size_t size) = 0;
+		virtual bool finishedImpl() { return false; }
+
+		std::atomic<State> m_state = State::Idle;
+		std::vector<SampleFrame> m_buffer;
+		AudioGraph* m_graph = nullptr;
+
+		friend class AudioGraph;
+	};
+
+	AudioGraph();
+	AudioGraph(const AudioGraph&) = delete;
+	AudioGraph(AudioGraph&&) = delete;
+	AudioGraph& operator=(const AudioGraph&) = delete;
+	AudioGraph& operator=(AudioGraph&&) = delete;
+	~AudioGraph() noexcept;
+
+	void add(Node* node);
+	void remove(Node* node);
+	void route(Node* from, Node* to);
+	void unroute(Node* from, Node* to);
+	void reroute(Node* oldFrom, Node* oldTo, Node* newFrom, Node* newTo);
+	void process();
+
+	static AudioGraph& inst();
+
+private:
+	class Worker
+	{
+	public:
+		Worker(AudioGraph* graph);
+		~Worker() noexcept = default;
+
+		Worker(Worker&&) noexcept;
+		Worker& operator=(Worker&&) noexcept;
+
+		Worker(const Worker&) = delete;
+		Worker& operator=(const Worker&) = delete;
+
+	private:
+		void runWorker(AudioGraph* graph);
+
+		static constexpr auto MaxWorkPerWorker = 512;
+		std::vector<Node*> m_workStack;
+		std::atomic<int> m_workStackIndex = 0;
+
+		std::thread m_thread;
+		friend class AudioGraph;
+	};
+
+	std::vector<Node*> m_nodes;
+	std::unordered_map<Node*, std::vector<Node*>> m_dependencies;
+	std::unordered_map<Node*, std::vector<Node*>> m_dependents;
+
+	std::vector<Worker> m_workers;
+	std::atomic<bool> m_quit = false;
+	std::atomic<std::size_t> m_nodesLeftToProcess = 0;
+};
+} // namespace lmms
+
+#endif // LMMS_AUDIO_GRAPH_H
---
+++ src/core/audio/AudioGraph.cpp
@@ -0,0 +1,189 @@
+/*
+ * AudioGraph.cpp
+ *
+ * Copyright (c) 2025 Sotonye Atemie <sakertooth@gmail.com>
+ *
+ * This file is part of LMMS - https://lmms.io
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program (see COPYING); if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301 USA.
+ *
+ */
+
+#include "AudioGraph.h"
+
+#include "AudioEngine.h"
+#include "Engine.h"
+
+namespace lmms {
+
+AudioGraph::AudioGraph()
+{
+	for (auto i = std::size_t{0}; i < std::thread::hardware_concurrency() - 1; ++i)
+	{
+		m_workers.emplace_back(this);
+	}
+}
+
+AudioGraph::~AudioGraph() noexcept
+{
+	m_quit = true;
+
+	for (auto& worker : m_workers)
+	{
+		if (worker.m_thread.joinable()) { worker.m_thread.join(); }
+	}
+}
+
+void AudioGraph::add(Node* node)
+{
+	if (node->m_graph == this) { return; }
+
+	const auto guard = Engine::audioEngine()->requestChangesGuard();
+	m_nodes.push_back(node);
+	node->m_graph = this;
+}
+
+void AudioGraph::remove(Node* node)
+{
+	if (node->m_graph != this) { return; }
+
+	const auto guard = Engine::audioEngine()->requestChangesGuard();
+	m_nodes.erase(std::remove(m_nodes.begin(), m_nodes.end(), node));
+	m_dependencies.erase(node);
+	m_dependents.erase(node);
+	node->m_graph = nullptr;
+}
+
+void AudioGraph::route(Node* from, Node* to)
+{
+	if (from->m_graph != this || to->m_graph != this) { return; }
+
+	const auto guard = Engine::audioEngine()->requestChangesGuard();
+	m_dependencies[to].emplace_back(from);
+	m_dependents[from].emplace_back(to);
+}
+
+void AudioGraph::unroute(Node* from, Node* to)
+{
+	if (from->m_graph != this || to->m_graph != this) { return; }
+
+	const auto guard = Engine::audioEngine()->requestChangesGuard();
+	m_dependencies[to].erase(std::remove(m_dependencies[to].begin(), m_dependencies[to].end(), from));
+	m_dependents[from].erase(std::remove(m_dependents[from].begin(), m_dependents[from].end(), to));
+}
+
+void AudioGraph::reroute(Node* oldFrom, Node* oldTo, Node* newFrom, Node* newTo)
+{
+	const auto guard = Engine::audioEngine()->requestChangesGuard();
+	unroute(oldFrom, oldTo);
+	route(newFrom, newTo);
+}
+
+AudioGraph::Node::~Node() noexcept
+{
+	if (m_graph) { m_graph->remove(this); }
+}
+
+AudioGraph& AudioGraph::inst()
+{
+	static auto s_inst = AudioGraph{};
+	return s_inst;
+}
+
+void AudioGraph::process()
+{
+	m_nodesLeftToProcess = m_nodes.size();
+	auto nextAssignedWorker = std::size_t{0};
+
+	while (m_nodesLeftToProcess < 0)
+	{
+		for (auto& node : m_nodes)
+		{
+			const auto isReady = std::all_of(m_dependencies[node].begin(), m_dependencies[node].end(), [](Node* other) {
+				return other->m_state == Node::State::Processed;
+			}) && node->m_state == Node::State::Idle;
+
+			if (isReady)
+			{
+				node->m_state = Node::State::Queued;
+
+				auto& worker = m_workers[nextAssignedWorker];
+				worker.m_workStackIndex.wait(worker.m_workStack.size());
+				worker.m_workStack[worker.m_workStackIndex] = node;
+
+				worker.m_workStackIndex.fetch_add(1);
+				worker.m_workStackIndex.notify_one();
+				nextAssignedWorker = (nextAssignedWorker + 1) % m_workers.size();
+			}
+		}
+
+		m_nodesLeftToProcess.wait(m_nodesLeftToProcess);
+	}
+
+	for (auto& node : m_nodes)
+	{
+		if (node->finished()) { remove(node); }
+		node->m_state = Node::State::Idle;
+	}
+}
+
+AudioGraph::Worker::Worker(AudioGraph* graph)
+	: m_workStack(MaxWorkPerWorker)
+	, m_thread(&Worker::runWorker, this, graph)
+{
+}
+
+AudioGraph::Worker::Worker(Worker&& other) noexcept
+	: m_workStack(std::move(other.m_workStack))
+	, m_thread(std::move(other.m_thread))
+{
+}
+
+AudioGraph::Worker& AudioGraph::Worker::operator=(Worker&& other) noexcept
+{
+	m_workStack = std::move(other.m_workStack);
+	m_thread = std::move(other.m_thread);
+	return *this;
+}
+
+void AudioGraph::Worker::runWorker(AudioGraph* graph)
+{
+	while (!graph->m_quit)
+	{
+		m_workStackIndex.wait(0);
+
+		const auto node = m_workStack[m_workStackIndex.fetch_sub(1)];
+
+		for (auto& dependency : graph->m_dependencies[node])
+		{
+			dependency->send(node);
+		}
+
+		node->m_state = Node::State::Processing;
+		node->process();
+
+		for (auto& dependent : graph->m_dependents[node])
+		{
+			node->send(dependent);
+		}
+
+		node->m_state = Node::State::Processed;
+		graph->m_nodesLeftToProcess.fetch_sub(1, std::memory_order_relaxed);
+		graph->m_nodesLeftToProcess.notify_one();
+	}
+}
+
+} // namespace lmms
