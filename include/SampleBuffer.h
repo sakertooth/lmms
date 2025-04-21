00780d45780b02c2d24a709a7fa630d9d5d4370d
saker <sakertooth@gmail.com>
2025-04-21 10:36:11 -0400

    Create new SampleBuffer API

--- include/SampleBuffer.h
+++ include/SampleBuffer.h
@@ -1,5 +1,5 @@
 /*
- * SampleBuffer.h - container-class SampleBuffer
+ * SampleBuffer.h
  *
  * Copyright (c) 2005-2014 Tobias Doerffel <tobydox/at/users.sourceforge.net>
  *
@@ -25,73 +25,83 @@
 #ifndef LMMS_SAMPLE_BUFFER_H
 #define LMMS_SAMPLE_BUFFER_H
 
-#include <QByteArray>
-#include <QString>
-#include <memory>
-#include <optional>
-#include <samplerate.h>
+#include <chrono>
+#include <filesystem>
 #include <vector>
 
-#include "AudioEngine.h"
-#include "Engine.h"
-#include "LmmsTypes.h"
 #include "lmms_export.h"
 
 namespace lmms {
+
+//! A class that represents and stores a non-interleaved (planar) audio buffer.
+//! The buffer stores its samples in the form of LLLRRR inside one contiguous buffer on the heap.
 class LMMS_EXPORT SampleBuffer
 {
 public:
-	using value_type = SampleFrame;
-	using reference = SampleFrame&;
-	using const_reference = const SampleFrame&;
-	using iterator = std::vector<SampleFrame>::iterator;
-	using const_iterator = std::vector<SampleFrame>::const_iterator;
-	using difference_type = std::vector<SampleFrame>::difference_type;
-	using size_type = std::vector<SampleFrame>::size_type;
-	using reverse_iterator = std::vector<SampleFrame>::reverse_iterator;
-	using const_reverse_iterator = std::vector<SampleFrame>::const_reverse_iterator;
+	SampleBuffer(const SampleBuffer&);
+	SampleBuffer& operator=(const SampleBuffer&);
+	SampleBuffer(SampleBuffer&&) noexcept;
+	SampleBuffer& operator=(SampleBuffer&&) noexcept;
 
-	SampleBuffer() = default;
-	explicit SampleBuffer(const QString& audioFile);
-	SampleBuffer(const QString& base64, int sampleRate);
-	SampleBuffer(std::vector<SampleFrame> data, int sampleRate);
-	SampleBuffer(
-		const SampleFrame* data, size_t numFrames, int sampleRate = Engine::audioEngine()->outputSampleRate());
+	//! Creates an empty buffer.
+	SampleBuffer(int numChannels, int numFrames, int sampleRate);
 
-	friend void swap(SampleBuffer& first, SampleBuffer& second) noexcept;
-	auto toBase64() const -> QString;
+	//! Creates a buffer from interleaved audio data contained within @p src.
+	//! The layouts will be converted as necessary.
+	SampleBuffer(const float* src, int numChannels, int numFrames, int sampleRate);
 
-	auto audioFile() const -> const QString& { return m_audioFile; }
-	auto sampleRate() const -> sample_rate_t { return m_sampleRate; }
+	//! Creates a buffer from planar audio data contained within @p src.
+	SampleBuffer(const float** src, int numChannels, int numFrames, int sampleRate);
 
-	auto begin() -> iterator { return m_data.begin(); }
-	auto end() -> iterator { return m_data.end(); }
+	//! Creates a buffer by loading data from the given audio file @p path.
+	//! If the given sample rate does not match the sample rate of the audio file, the sample rate is
+	//! converted.
+	SampleBuffer(const std::filesystem::path& path, int sampleRate);
 
-	auto begin() const -> const_iterator { return m_data.begin(); }
-	auto end() const -> const_iterator { return m_data.end(); }
+	//! Destroys the buffer.
+	~SampleBuffer() = default;
 
-	auto cbegin() const -> const_iterator { return m_data.cbegin(); }
-	auto cend() const -> const_iterator { return m_data.cend(); }
+	//! Returns the plane at the given channel index.
+	float* operator[](std::size_t channel) { return m_planes[channel]; }
 
-	auto rbegin() -> reverse_iterator { return m_data.rbegin(); }
-	auto rend() -> reverse_iterator { return m_data.rend(); }
+	//! Returns the plane at the given channel index.
+	const float* operator[](std::size_t channel) const { return m_planes[channel]; }
 
-	auto rbegin() const -> const_reverse_iterator { return m_data.rbegin(); }
-	auto rend() const -> const_reverse_iterator { return m_data.rend(); }
+	//! Converts the stored planar audio data into an interleaved form stored in @p dst.
+	//! @p dst is expected to have the same size as the buffer.
+	void convertToInterleaved(float* dst) const;
 
-	auto crbegin() const -> const_reverse_iterator { return m_data.crbegin(); }
-	auto crend() const -> const_reverse_iterator { return m_data.crend(); }
+	//! Return the planes (0: LLL, 1: RRR, etc) for this buffer.
+	auto data() -> float** { return m_planes.data(); }
 
-	auto data() const -> const SampleFrame* { return m_data.data(); }
-	auto size() const -> size_type { return m_data.size(); }
-	auto empty() const -> bool { return m_data.empty(); }
+	//! Return the planes (0: LLL, 1: RRR, etc) for this buffer.
+	auto data() const -> const float* const*  { return m_planes.data(); }
 
-	static auto emptyBuffer() -> std::shared_ptr<const SampleBuffer>;
+	//! Calculate the duration of the buffer.
+	auto duration() const -> std::chrono::milliseconds;
+
+	//! Returns the number of the channels this buffer contains.
+	auto numChannels() const -> int { return m_numChannels; }
+
+	//! Returns the number of frames this buffer contains.
+	//! A frame represents a collection of samples, one for each channel.
+	auto numFrames() const -> int { return m_numFrames; }
+
+	//! Add @p src1 and @p src2 element-wise and store the results in @p dst.
+	//! The buffers can overlap.
+	//! It is assumed that all buffers are of the same size.
+	static void add(SampleBuffer& dst, const SampleBuffer& src1, const SampleBuffer& src2);
+
+	//! Do an element-wise multiplication with the samples in @p dst with a buffer of elements inside @p src.
+	//! Both @p dst and @p src are expected to have a size of @p size.
+	static void multiply(SampleBuffer& dst, const float* src, std::size_t size);
 
 private:
-	std::vector<SampleFrame> m_data;
-	QString m_audioFile;
-	sample_rate_t m_sampleRate = Engine::audioEngine()->outputSampleRate();
+	std::vector<float> m_data;
+	std::vector<float*> m_planes;
+	int m_numChannels;
+	int m_numFrames;
+	int m_sampleRate;
 };
 
 } // namespace lmms
